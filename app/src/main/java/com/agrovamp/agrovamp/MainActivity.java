c0311f906d6c8afa89b3fabc7491e0718ef18f38
agrovamp <agrovamp@gmail.com>
2018-03-13 19:58:35 +0530

    Example commit

--- app/src/main/java/com/agrovamp/agrovamp/MainActivity.java
+++ app/src/main/java/com/agrovamp/agrovamp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.agrovamp.agrovamp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -13,13 +15,20 @@ import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String LANG_KEY = "LANG_KEY";
 
     private Spinner languageSpinner;
     private Button nextButton;
 
     private FirebaseAuth firebaseAuth;
     private FirebaseUser firebaseUser;
+
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +42,9 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(this, UserMainActivity.class));
             finish();
         }
+
+        preferences = getPreferences(MODE_PRIVATE);
+        editor = preferences.edit();
 
         languageSpinner = (Spinner) findViewById(R.id.language_spinner);
         nextButton = (Button) findViewById(R.id.next_button);
@@ -48,8 +60,24 @@ public class MainActivity extends AppCompatActivity {
                 if (TextUtils.isEmpty(language)) {
                     Toast.makeText(MainActivity.this, R.string.select_a_language, Toast.LENGTH_SHORT).show();
                 } else {
+                    String language_code = "en";
+                    switch (language) {
+                        case "English": language_code = "en";
+                            break;
+                        case "Hindi": language_code = "hi";
+                            break;
+                        case "Marathi": language_code = "mr";
+                            break;
+                    }
+                    editor.putString(LANG_KEY, language_code);
+                    editor.commit();
                     Toast.makeText(MainActivity.this, getString(R.string.user_selected) + language, Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(MainActivity.this, MobileNumberActivity.class));
+                    startActivity(new Intent(MainActivity.this, UserMainActivity.class).putExtra(LANG_KEY, language_code));
+
+                    Configuration configuration = getResources().getConfiguration();
+                    configuration.setLocale(new Locale(language_code));
+                    getBaseContext().getResources().updateConfiguration(configuration, getBaseContext().getResources().getDisplayMetrics());
+                    recreate();
                     finish();
                 }
             }
